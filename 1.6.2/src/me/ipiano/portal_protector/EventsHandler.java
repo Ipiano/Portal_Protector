b8226d33972ba643c6a674bd628001df6724cfc0
Ipiano <andrew@stelterfamily.net>
2013-09-14 00:17:32 -0500

    1.6.1-1.6.2

---
+++ 1.6.2/src/me/ipiano/portal_protector/EventsHandler.java
@@ -0,0 +1,291 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ipiano.portal_protector;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.ThrownPotion;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+
+/**
+ *
+ * @author Andrew
+ */
+public class EventsHandler implements Listener{
+    public static PortalProtector plugin;
+
+    public final static String meleePath = "protection.melee";
+    public final static String projectilePath = "protection.projectile";
+    public final static String blockPath = "protection.blockplace";
+    public final static String obsidianPath = "protection.obsidianplace";
+    public final static String firePath = "protection.firestart";
+    public final static String lavaPlacePath = "protection.lavaplace";
+    public final static String lavaFlowPath = "protection.lavaflow";
+    public final static String potionPath = "protection.potionsplash";
+    public final static String tntPath = "protection.tntexplode";
+    private final int RADIUS = 2;
+
+   
+
+    
+    public EventsHandler(PortalProtector instance)
+    {
+        plugin = instance;
+    }
+    
+    //BEGIN EVENT HANDLERS
+    
+    //Handler for melee and projectile damage
+    @EventHandler
+    public void onTakeDamage(EntityDamageByEntityEvent event){
+        Entity entity = event.getEntity();
+        boolean validEvent = false;
+        Player player = null;
+        Player damager = null;
+        String path = meleePath;
+
+        //If it was melee damage, flag the player and damager
+        if(entity instanceof Player && event.getDamager() instanceof Player){
+            player = (Player)entity;
+            damager = (Player)event.getDamager();
+            validEvent = true;
+            path = meleePath;
+
+        //If it was projectile damage, flag the player and the shooter of the projectile
+        }else if(event.getDamager() instanceof Projectile){
+            Projectile project = (Projectile) event.getDamager();
+            if(entity instanceof Player && project.getShooter() instanceof Player){
+                player = (Player)entity;
+                damager = (Player)project.getShooter();
+                project.remove();
+                validEvent = true;
+                path = projectilePath;
+            }
+        }
+        if(plugin.getProperties().getBoolean(path)){        
+            if(validEvent){
+
+                //If either player is in range of a portal, and the damager and damagee were players, cancel the damage
+
+                if(inRangeOfPortal(player.getLocation()) || inRangeOfPortal(damager.getLocation())){
+                        //PortalProtector.m_log.info("Player [" + player.getDisplayName() + "] is near a portal; negating " + event.getDamage() + " damage");
+                        notifyPlayer(damager, ChatColor.DARK_RED + "You cannot attack this player right now, one of you is too close to a portal.");
+                        event.setCancelled(true);
+                }
+            }
+            
+        }
+    }    
+    
+    //Handler for placement of blocks
+    @EventHandler
+    public void onBuild(BlockPlaceEvent event){
+        Player plr = event.getPlayer();
+        Location loc = event.getBlock().getLocation();
+        if(inRangeOfPortal(loc, 1,0,0) || inRangeOfPortal(loc, 0,0,1)){
+            if(event.getBlock().getType() == Material.OBSIDIAN && plugin.getProperties().getBoolean(obsidianPath)){
+                
+                //Prevent any obsidian being placed 1 away from a portal block
+                
+                event.setCancelled(true);
+                notifyPlayer(plr, ChatColor.DARK_RED + "You can't put obsidan that close to a portal!");
+                plr.updateInventory();
+            }else{
+                if(plugin.getProperties().getBoolean(blockPath)){
+                    if(isPlayerInPortal(loc)){
+
+                        //Prevent any block being placed by a portal block if a nearby player is standing in a portal
+                        //Could potentially prevent unecessary blocks when portals are excessively close(1-3 blocks away)
+
+                        event.setCancelled(true);
+                        notifyPlayer(plr, ChatColor.DARK_RED + "You can't put blocks near a portal someone is in");
+                        plr.updateInventory();
+ 
+                    }
+                        
+                }
+            }
+        }
+    }
+    
+    //Handler for lighting fire near portals
+    @EventHandler
+    public void onBurn(BlockIgniteEvent event){
+        if(plugin.getProperties().getBoolean(firePath)){       
+            Entity entity = event.getPlayer();
+            if(null != entity){
+                Player plr = (Player)entity;
+                Location loc = event.getBlock().getLocation();
+                if(inRangeOfPortal(loc, 1,1,1)){
+                        event.setCancelled(true);
+                        notifyPlayer(plr,ChatColor.DARK_RED + "You can't start a fire that close to a portal.");
+                        plr.updateInventory();
+                }
+            }
+        }
+    }
+    
+    //Handler for placement of lava
+    @EventHandler
+    public void onDrop(PlayerBucketEmptyEvent event){
+        if(plugin.getProperties().getBoolean(lavaPlacePath)){
+            Entity entity = event.getPlayer();
+            if(event.getBucket() == Material.LAVA_BUCKET){
+                if(null != entity){
+                    Player plr = (Player)entity;
+                    Location loc = event.getBlockClicked().getLocation().add(event.getBlockFace().getModX(), event.getBlockFace().getModY(),event.getBlockFace().getModZ());
+                    if(loc.getBlock().getType() == Material.PORTAL && !isPlayerInPortal(loc, 1,1,1)){
+                        //Allows breaking of portals with lava when players are not in them
+                    }else if(inRangeOfPortal(loc) || event.getBlockClicked().getType() == Material.PORTAL){
+                            event.setCancelled(true);
+                            plr.sendBlockChange(loc, Material.AIR, loc.getBlock().getData());
+                            notifyPlayer(plr,ChatColor.DARK_RED + "You can't put lava there, it's too close to a portal!");
+                            plr.updateInventory();
+                    }
+                }
+            }
+        }
+    }
+    
+    //Handler for flowing lava near portals
+    @EventHandler
+    public void onFlow(BlockFromToEvent event){
+    //If any lava attempts to flow near or into a space near a portal, the block that caused the event is removed and the lava does not flow
+    //Will remove pre-existing lava from near portals over time, but only if the lava is disturbed, causing a reason for it to flow
+        if(plugin.getProperties().getBoolean(lavaFlowPath)){
+            Material startType = event.getBlock().getType();
+            Material lava = Material.STATIONARY_LAVA;
+            Material lavaFlow = Material.LAVA;
+
+            if((startType == lava || startType == lavaFlow) && inRangeOfPortal(event.getBlock().getLocation())){
+                    event.getBlock().setType(Material.AIR);
+                    event.setCancelled(true);
+            }
+        }
+    }  
+    
+    //Handler for potion splashes
+    @EventHandler
+    public void onMakeSplash(PotionSplashEvent event){
+        if(plugin.getProperties().getBoolean(potionPath)){
+            Entity entity = event.getEntity();
+            if (entity instanceof ThrownPotion){
+                ArrayList<Player> plrsAffected = new ArrayList();
+                ThrownPotion potion = (ThrownPotion)entity;
+                boolean validEvent = false;
+                Player damager = null;
+                if(potion.getShooter() instanceof Player){
+                    LivingEntity[] affected = event.getAffectedEntities().toArray(new LivingEntity[event.getAffectedEntities().size()]);
+                    damager = (Player)potion.getShooter();
+                    for(int i=0; i<affected.length;i++){
+                        if(affected[i] instanceof Player){
+                            plrsAffected.add((Player)affected[i]);
+                        }
+                    }
+                    validEvent = true;
+                }
+
+                //If any players are affected, checks if any are in range of a portal
+                //If the thrower is in range of a portal, all effects on players are nullified
+                //If it would affects player(s) near a portal, only that player(s) will have the effect nullified
+                if(validEvent){
+                    Player[] finalPlrs = plrsAffected.toArray(new Player[plrsAffected.size()]);
+                    if(inRangeOfPortal(damager.getLocation())){
+                        //PortalProtector.m_log.info("Player [" + player.getDisplayName() + "] is near a portal; negating " + event.getDamage() + " damage");
+                        notifyPlayer(damager, ChatColor.DARK_RED + "You cannot attack this player right now, you are too close to a portal.");
+                        for (int i = 0; i < finalPlrs.length; i++){
+                            event.setIntensity(finalPlrs[i], 0);
+                        }
+                    }else{
+                        boolean message = false;
+                        for(int i = 0; i < finalPlrs.length; i++){
+                            if(inRangeOfPortal(finalPlrs[i].getLocation())){
+                                event.setIntensity(finalPlrs[i], 0);
+                                message = true;
+                            }
+                        }
+                        if (message){
+                            notifyPlayer(damager, ChatColor.DARK_RED + "Some players were not affected by the potion because they were near a portal.");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    
+    //Handler for TNT explosions
+    @EventHandler
+    public void onPrime(EntityExplodeEvent event){
+        if(plugin.getProperties().getBoolean(tntPath)){
+          Entity thisPrime = event.getEntity();
+            if(thisPrime.getType() == EntityType.PRIMED_TNT && inRangeOfPortal(thisPrime.getLocation(), 7,7,7)){
+                event.setCancelled(true);
+            }
+        }
+
+    }
+    
+    
+    //END EVENT HANDLERS
+    //BEGIN HELPER METHODS
+    public boolean isPlayerInPortal(Location loc){
+        return isPlayerInPortal(loc, 0,0,0);
+    }
+    public boolean isPlayerInPortal(Location loc, int x, int y, int z){
+        List<Entity> near = loc.getWorld().getEntities();
+        for(Entity e : near) {
+            if(e.getLocation().distance(loc) <= 2 && e instanceof Player){
+                if(inRangeOfPortal(e.getLocation(), x, y, z)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    //Method to check if a location is in range of a portal, default distance is the RADIUS constant
+    public boolean inRangeOfPortal(Location location){
+        return inRangeOfPortal(location, RADIUS, RADIUS, RADIUS);
+    }
+    public boolean inRangeOfPortal(Location location, int xVar, int yVar, int zVar){
+        for (int x = -(xVar); x <= xVar; x++){
+            for (int y = -(yVar); y <= yVar; y++){
+                for (int z = -(zVar); z <= zVar; z++){
+                    Location loc = location.getBlock().getRelative(x, y, z).getLocation();
+                    if (loc.getBlock().getType() == Material.PORTAL){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    public void notifyPlayer(Player player, String message){
+        try{
+            if(plugin.getMetaboolean(player, PortalProtector.m_notifyKey, plugin)){
+                player.sendMessage(message);
+            }
+        }catch(Exception ex){player.sendMessage(message);}
+    }
+    
+}
---
+++ 1.6.2/src/me/ipiano/portal_protector/PortalProtector.java
@@ -0,0 +1,215 @@
+package me.ipiano.portal_protector;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Ipiano
+ */
+
+//Main class, registers all events which must be modified
+public class PortalProtector extends JavaPlugin {
+    //Distance away from portal for most effects
+    public static final Logger m_log = Logger.getLogger("Minecraft");
+    public static final String m_notifyKey = "portalprotector.notify";
+    public static final HashMap<String, String> m_pathMap = new HashMap<String, String>();
+
+    
+    //BEGIN SERVER METHODS
+    @Override
+    public void onEnable(){
+        m_log.info("PortalProtector is active");
+        PluginManager manager = getServer().getPluginManager();
+        getDataFolder().mkdir();
+        saveDefaultConfig();
+        
+        m_pathMap.put("melee","Melee");
+        m_pathMap.put("projectile","Projectile");
+        m_pathMap.put("blockplace","Block placement(When a player is in the portal)");
+        m_pathMap.put("obsidianplace","Obsidian placement(Adjecent to portals)");
+        m_pathMap.put("firestart","Starting fires");
+        m_pathMap.put("lavaplace","Lava placement");
+        m_pathMap.put("lavaflow","Lava flowing");
+        m_pathMap.put("potionsplash","Potion Splash");
+        m_pathMap.put("tntexplode","TNT exploding");
+        
+        //Registers the listener classes
+        manager.registerEvents(new EventsHandler(this), this);
+
+        
+    }
+    
+    @Override
+    public void onDisable(){
+        getLogger().info("PortalProtector is inactive");        
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	if (cmd.getName().equalsIgnoreCase("PortalProtector")){
+            if(args.length == 1){
+                if(args[0].equalsIgnoreCase("notify")){
+                    
+                    if(sender instanceof Player){
+                        
+                        //Notify command switches the notification status for a player and tells them what it is. Does not save over server reboot, defaults to notifications on
+                        
+                        Player thisPlr = (Player)sender;
+                        boolean curVal = true;
+                        try{
+                            curVal = getMetaboolean(thisPlr, m_notifyKey, this);
+                        }catch (Exception ex){m_log.info(ex.getMessage());}
+                            setMetadata(thisPlr, m_notifyKey, !curVal, this);
+                            String result = ChatColor.GREEN + "on";
+                            if(curVal){
+                                result = ChatColor.RED + "off";
+                            }
+                            sender.sendMessage(ChatColor.DARK_PURPLE +"PortalProtector "+ ChatColor.WHITE +"notifications are now " + result);
+
+
+                    }else{
+                        
+                        //Doesn't allow the CommandWindow to use this command
+                        
+                        sender.sendMessage(ChatColor.RED +"Only players can do this");
+                    }
+                    return true;
+                }else if(args[0].equalsIgnoreCase("status")){
+                    
+                    //Status command displays the status of all plugin effects
+                    
+                    sender.sendMessage(ChatColor.BLACK + "-----" +ChatColor.DARK_PURPLE +"PortalProtector STATUS" + ChatColor.BLACK + "-----");
+                    sender.sendMessage("These effects are only in place near portals");
+                    for(Entry<String, String> entry : m_pathMap.entrySet()){
+                        boolean setting = getConfig().getBoolean("protection." + entry.getKey());
+                        String output = (ChatColor.GREEN + "protected");
+                        if(!setting){
+                            output = (ChatColor.RED + "not protected");
+                        }
+                        sender.sendMessage(ChatColor.DARK_PURPLE + entry.getValue() + ChatColor.WHITE + " is " + output);
+                    }
+                    return true;
+                }else if(args[0].equalsIgnoreCase("refresh")){
+                    
+                    //Refresh command reloads the config.yml file, op players only
+                    
+                    if(sender.hasPermission("portalprotector.manager") || sender instanceof ConsoleCommandSender){
+                        reloadConfig();
+                        sender.sendMessage("PortalProtector refreshed");
+                    }else{
+                        sender.sendMessage(ChatColor.DARK_RED +"You don't have permission to do that");
+                    }
+                    return true;
+                }else if(args[0].equalsIgnoreCase("help")){
+                    
+                    //The help command lists what plugin commands the user can use
+                            
+                    sender.sendMessage(ChatColor.BLACK + "-----" +ChatColor.DARK_PURPLE +"PortalProtector HELP" + ChatColor.BLACK + "-----");
+                    if(!(sender instanceof ConsoleCommandSender)){
+                        sender.sendMessage(ChatColor.DARK_PURPLE +"/PortalProtector notify"+ChatColor.WHITE+" - Toggle notification when disallowed actions are attempted");
+                    }
+                    sender.sendMessage(ChatColor.DARK_PURPLE +"/PortalProtector status"+ChatColor.WHITE+" - See what is currently protected by the plugin");
+                    if(sender.hasPermission("portalprotector.manager") || sender instanceof ConsoleCommandSender){
+                        sender.sendMessage(ChatColor.DARK_PURPLE +"/PortalProtector refresh"+ChatColor.WHITE+" - Refresh the plugin to load changes to config.yml");
+                        sender.sendMessage(ChatColor.DARK_PURPLE +"/PortalProtector <effect><setting>"+ChatColor.WHITE+" - Set whether the plugin protects an event");
+                        sender.sendMessage(ChatColor.BLUE+"Example: "+ChatColor.DARK_PURPLE +"/PortalProtector melee unprotect" + ChatColor.WHITE +" would disable melee protection");
+                        sender.sendMessage("You can use either " + ChatColor.GREEN + "protect" + ChatColor.WHITE + " or " + ChatColor.RED + "unprotect" + ChatColor.WHITE + " as the setting");
+                        sender.sendMessage("For a list of effects that can be changed type " + ChatColor.DARK_PURPLE + "/PortalProtector effects");
+                        
+                    }
+                    return true;
+                }else if(args[0].equalsIgnoreCase("effects")){
+                    
+                    //Effects lists the name of each affect for the purpose of the <affect><setting> command
+                    
+                    if(sender.hasPermission("portalprotector.manager") || sender instanceof ConsoleCommandSender){
+                        String finalOutput = "The PortalProtector effects are as follows:\n";
+                        for(Entry<String, String> entry : m_pathMap.entrySet()){
+                            finalOutput += ChatColor.DARK_PURPLE + entry.getKey() + "\n";
+                        }
+                        sender.sendMessage(finalOutput);
+                    }else{
+                        sender.sendMessage(ChatColor.DARK_RED +"You don't have permission to do that");
+                    }
+                    return true;
+                }
+            }else if(args.length == 2){
+                if(sender.hasPermission("portalprotector.manager")|| sender instanceof ConsoleCommandSender){
+                    
+                    //<effect><setting> command allows op players to change the config.yml from in-game
+                    
+                    if(getConfig().contains("protection." + args[0])){
+                        try{
+                            boolean var = parseBoolean(args[1]);
+                            getConfig().set("protection." + args[0], var);
+                            saveConfig();
+                            String output = ChatColor.GREEN + "protected";
+                            if(!var){
+                                output = ChatColor.RED + "unprotected";
+                            }
+                            sender.sendMessage(ChatColor.DARK_PURPLE + args[0] + ChatColor.WHITE + " is "  + output);
+                            return true;
+                        }catch(Exception ex){
+                            sender.sendMessage("Enter"+ ChatColor.GREEN + " protect " + ChatColor.WHITE +"or" + ChatColor.RED+" unprotect " + ChatColor.WHITE+"after an effect name, to see effect names use" + ChatColor.DARK_PURPLE + "/PortalProtector effects");
+                            return true;
+                        }
+                    }
+                }else{
+                    sender.sendMessage(ChatColor.DARK_RED +"You don't have permission to do that");
+                    return true;                    
+                }
+                    
+            }
+        }
+        return false;
+
+    }
+    
+    public FileConfiguration getProperties(){
+        return getConfig();
+    }
+
+    //END SERVER METHODS
+    //BEGIN HELPER METHODS
+    
+    public void setMetadata(Player player, String key, Object value, Plugin plugin){
+        player.setMetadata(key,new FixedMetadataValue(plugin,value));
+    }
+    public boolean getMetaboolean(Player player, String key, Plugin plugin) throws Exception{
+        Player[] online = getServer().getOnlinePlayers();
+        for(int i =0; i<online.length; i++){
+            if (player == online[i]){
+                List<MetadataValue> values = player.getMetadata(key);  
+                for(MetadataValue value : values){
+                    if(value.getOwningPlugin().getDescription().getName().equals(plugin.getDescription().getName())){
+                        return value.asBoolean();
+                    }
+                }
+            }
+        }
+        throw new Exception("That value or player doesn't exist");
+    } 
+    
+    public boolean parseBoolean(String val) throws Exception  {
+        if(val.equalsIgnoreCase("true") || val.equalsIgnoreCase("protect")){
+            return true;
+        }else if(val.equalsIgnoreCase("false") || val.equalsIgnoreCase("unprotect")){
+            return false;
+        }
+        throw new Exception();
+    }
+}
